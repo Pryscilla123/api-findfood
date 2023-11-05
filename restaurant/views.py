e4f62d8a2660aeba4e21e01bcca317de2b5177d6
Pryscilla Medeiros <pryscilla.medeiros@enacom.com.br>
2023-11-05 20:59:24 -0300

    terminando rotas da api

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render
-from .serializers import RestaurantSerializer, AddressSerializer, RestaurantInformationSerializer, ScheduleSerializer
-from .models import Restaurant, Address, Schedule, Interval
+from .serializers import RestaurantSerializer, AddressSerializer, RestaurantInformationSerializer, ScheduleSerializer, \
+    ContactSerializer
+from .models import Restaurant, Address, Schedule, Contact
 from rest_framework import status
-from rest_framework.mixins import CreateModelMixin, UpdateModelMixin, DestroyModelMixin, ListModelMixin
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import CreateModelMixin, UpdateModelMixin, DestroyModelMixin
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework.response import Response
 
 
 # Create your views here.
 
 
-class RestaurantViewSet(CreateModelMixin, UpdateModelMixin, DestroyModelMixin, GenericViewSet):
+class RestaurantViewSet(ModelViewSet):
     """
     Rota de create, update e delete do Restaurante.
     """
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'list' or self.action == 'retrieve':
+            return RestaurantInformationSerializer
+        return RestaurantSerializer
 
 
 class AddressViewSet(CreateModelMixin, UpdateModelMixin, DestroyModelMixin, GenericViewSet):
@@ -35,66 +41,24 @@ class ScheduleViewSet(CreateModelMixin, UpdateModelMixin, DestroyModelMixin, Gen
     lookup_field = 'interval_id__day'
 
     def create(self, request, *args, **kwargs):
-        """
-        Estrutura -> List[dict]
-        Estrutura do dict ->
-        {
-            "schedule": {
-                "day": str,
-                "open": str,
-                "close": str
-            },
-            "restaurant_id": int
-        },
-        :param request:
-        :param args:
-        :param kwargs:
-        :return:
-        """
-        intervals = self._create_interval(request.data)
-        serializer = self.serializer_class(data=intervals, many=True)
+        serializer = self.serializer_class(data=request.data, many=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    def update(self, request, *args, **kwargs):
-        interval = self._interval_to_update(request.data)
-        schedule = list(Schedule.objects.filter(**kwargs)).pop(0)
-        schedule.interval_id = interval
-        schedule.save()
 
-        return Response(status=status.HTTP_200_OK)
-
-    @staticmethod
-    def _create_interval(data):
-        schedule = []
-        for interval in data:
-            day = interval.get('schedule')
-            interval_obj = Interval.objects.filter(**day)
-            if not interval_obj:
-                new_interval_obj = Interval.objects.create(**day)
-                schedule.append({'restaurant_id': interval.get('restaurant_id'), 'interval_id': new_interval_obj.id})
-            else:
-                existing_interval_obj = list(interval_obj).pop(0)
-                schedule.append(
-                    {'restaurant_id': interval.get('restaurant_id'), 'interval_id': existing_interval_obj.id})
-        return schedule
-
-    @staticmethod
-    def _interval_to_update(data):
-        interval = Interval.objects.filter(**data)
-
-        if not interval:
-            new_interval_obj = Interval.objects.create(**data)
-            return new_interval_obj
-        else:
-            return list(interval).pop(0)
-
-
-class RestaurantInformationViewSet(ListModelMixin, GenericViewSet):
+class ContactViewSet(CreateModelMixin, UpdateModelMixin, DestroyModelMixin, GenericViewSet):
     """
-    Rota das Informações dos Restaurantes.
+    Rota de create, update e delete para as formas de contato do restaurant.
     """
-    queryset = Restaurant.objects.all()
-    serializer_class = RestaurantInformationSerializer
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, many=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
