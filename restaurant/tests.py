aab764ae8333558969e745da1271939824a860aa
Pryscilla Medeiros <pryscilla.medeiros@enacom.com.br>
2023-11-16 17:23:56 -0300

    finishing API

--- restaurant/tests.py
+++ restaurant/tests.py
@@ -1,3 +1,29 @@
+import pytest
+from decouple import config
+from rest_framework.test import APIRequestFactory
 from django.test import TestCase
 
+
 # Create your tests here.
+
+
+# @pytest.fixture
+# def factory():
+#     return APIRequestFactory()
+#
+#
+# @pytest.fixture
+# def user(factory):
+#     request = factory.post('/api/token/',
+#                            {'username': config('ADMINISTER_USER'), 'password': config('ADMINISTER_PASSWORD')},
+#                            format='json')
+#     return request
+#
+#
+# def test_create_restaurant(user):
+#     print(user)
+#
+#     assert True
+
+def test_pytest_working():
+    assert True == True
