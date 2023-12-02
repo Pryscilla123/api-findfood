b3c4350c4920af9df32daef435f7dae7336b5b8f
Pryscilla Medeiros <pryscilla.medeiros@enacom.com.br>
2023-12-02 12:22:23 -0300

    finalizando testes

--- restaurant/tests.py
+++ restaurant/tests.py
@@ -1,12 +1,9 @@
 import pytest
-from decouple import config
 from rest_framework import status
 from rest_framework.test import APIClient
-from rest_framework_simplejwt.tokens import AccessToken
 from django.contrib.auth.models import User
-from .views import RestaurantViewSet
-from django.test import TestCase
-
+from utils.tools import Days
+from datetime import datetime
 
 # Create your tests here.
 
@@ -57,14 +54,64 @@ def schedule_info():
             {
                 "interval_id": {
                     "day": day,
-                    "open": "12:00",
-                    "close": "16:00"
+                    "open": "12:00:00",
+                    "close": "16:00:00"
                 },
                 "restaurant_id": restaurant_id
             }
         ]
 
     return make
+
+
+@pytest.fixture
+def contact_info():
+    def make(restaurant_id):
+        return [
+            {
+                "type": "insta",
+                "information": "@bkinfos",
+                "restaurant_id": restaurant_id,
+            }
+        ]
+    return make
+
+
+@pytest.fixture
+def restaurant_data(restaurant_info, address_info, schedule_info, contact_info):
+    def make(restaurant_id, address_id, schedule_id, interval_id, contact_id, is_open):
+        restaurant_data = restaurant_info('bk', 'fastfood')
+        address_data = address_info('rua da lagoa', 'bairro caixoeira', restaurant_id)
+        address_data = {'id': address_id, **address_data}
+        del address_data['restaurant_id']
+        schedule_data = schedule_info('Quarta', restaurant_id)
+        schedule_data[0] = {'id': schedule_id, **schedule_data[0]}
+        del schedule_data[0]['restaurant_id']
+        schedule_data[0]["interval_id"] = {'id': interval_id, **schedule_data[0]["interval_id"]}
+        contact_data = contact_info(restaurant_id)
+        contact_data[0] = {'id': contact_id, **contact_data[0]}
+        del contact_data[0]['restaurant_id']
+        return [
+            {
+                "id": restaurant_id,
+                "name": restaurant_data["name"],
+                "type": restaurant_data["type"],
+                "restaurant_address": address_data,
+                "opening_days": schedule_data,
+                "socials": contact_data,
+                "is_open": is_open,
+                "img": 'image/upload/' + restaurant_data["img"],
+            }
+        ]
+    return make
+
+
+@pytest.fixture
+def contact(client, contact_info):
+    def make_contact(restaurant_id):
+        response = client.post('/restaurant/contact/', contact_info(restaurant_id), format="json")
+        return response
+    return make_contact
 
 
 @pytest.fixture
@@ -125,6 +172,70 @@ def test_restaurant_delete_request(restaurant, client):
 
 
 @pytest.mark.django_db
+def test_restaurant_get_request(client, restaurant_data, restaurant, address, schedule, contact):
+    restaurant = restaurant('bk', 'fastfood')
+    restaurant_id = restaurant.data.get("id")
+    address = address('rua da lagoa', 'bairro caixoeira', restaurant_id)
+    address_id = address.data.get("id")
+    schedule = schedule("Quarta", restaurant_id)
+    schedule_id = dict(schedule.data[0]).get("id")
+    interval_data = dict(dict(schedule.data[0])["interval_id"])
+    interval_id = interval_data.get("id")
+    contact = contact(restaurant_id)
+    contact_id = dict(contact.data[0]).get("id")
+
+    todays_day = Days[datetime.now().strftime('%A').upper()].value
+    todays_time = datetime.now().time()
+    open_day = interval_data.get("day")
+
+    open = datetime.strptime(interval_data.get("open"), "%H:%M:%S").time()
+    close = datetime.strptime(interval_data.get("close"), "%H:%M:%S").time()
+
+    if open <= todays_time <= close and todays_day == open_day:
+        is_open = True
+    else:
+        is_open = False
+
+    restaurant_expected = restaurant_data(restaurant_id, address_id, schedule_id, interval_id, contact_id, is_open)
+
+    response = client.get('/restaurant/management/')
+    response_data = dict(response.data[0])
+    response_data['restaurant_address'] = dict(response_data['restaurant_address'])
+    response_data['opening_days'][0] = dict(response_data['opening_days'][0])
+    response_data['opening_days'][0]['interval_id'] = dict(response_data['opening_days'][0]['interval_id'])
+    response_data['socials'][0] = dict(response_data['socials'][0])
+
+    assert response_data == restaurant_expected[0]
+
+
+@pytest.mark.django_db
+def test_restaurant_filter_is_open(client, restaurant, address, schedule, contact):
+    restaurant = restaurant('bk', 'fastfood')
+    restaurant_id = restaurant.data.get("id")
+    _ = address('rua da lagoa', 'bairro caixoeira', restaurant_id)
+    schedule = schedule("Quarta", restaurant_id)
+    _ = contact(restaurant_id)
+
+    interval_data = dict(dict(schedule.data[0])["interval_id"])
+
+    todays_day = Days[datetime.now().strftime('%A').upper()].value
+    todays_time = datetime.now().time()
+    open_day = interval_data.get("day")
+
+    open = datetime.strptime(interval_data.get("open"), "%H:%M:%S").time()
+    close = datetime.strptime(interval_data.get("close"), "%H:%M:%S").time()
+
+    if open <= todays_time <= close and todays_day == open_day:
+        is_open = 'true'
+    else:
+        is_open = 'false'
+
+    response = client.get(f'/restaurant/management/?is_open={is_open}')
+
+    assert response.status_code == status.HTTP_200_OK and len(response.data) > 0
+
+
+@pytest.mark.django_db
 def test_address_post_request(restaurant, address):
     restaurant = restaurant('bk', 'fastfood')
     response = address('rua do restaurante', 'bairro do restaurante', restaurant.data.get('id'))
@@ -172,7 +283,7 @@ def test_schedule_put_request(client, restaurant, schedule, schedule_info):
     schedule = schedule('Quarta', restaurant_id)
     schedule_data = dict(schedule.data[0])
     schedule_info = schedule_info('Quarta', restaurant_id)[0]
-    schedule_info.get('interval_id').update({'open': '10:00'})
+    schedule_info.get('interval_id').update({'open': '10:00:00'})
     response = client.put(f'/restaurant/schedule/{schedule_data.get("id")}/', schedule_info, format='json')
     assert response.status_code == status.HTTP_200_OK
 
@@ -184,7 +295,7 @@ def test_schedule_patch_request(client, restaurant, schedule):
     schedule = schedule('Quarta', restaurant_id)
     schedule_data = dict(schedule.data[0])
     response = client.patch(f'/restaurant/schedule/{schedule_data.get("id")}/',
-                            {'interval_id': {'day': 'Segunda', 'open': '10:00', 'close': '16:00'},
+                            {'interval_id': {'day': 'Segunda', 'open': '10:00:00', 'close': '16:00:00'},
                              'restaurant_id': restaurant_id}, format='json')
     assert response.status_code == status.HTTP_200_OK
 
@@ -199,3 +310,37 @@ def test_schedule_delete_request(client, restaurant, schedule):
     response_get_restaurant = client.get(f'/restaurant/management/{restaurant_2.data.get("id")}/')
     assert response.status_code == status.HTTP_204_NO_CONTENT and dict(
         response_get_restaurant.data.get('opening_days')[0]).get('interval_id')
+
+
+@pytest.mark.django_db
+def test_contact_post_request(contact, restaurant):
+    restaurant = restaurant('bk', 'fastfood')
+    contact = contact(restaurant.data.get("id"))
+    assert contact.status_code == status.HTTP_201_CREATED
+
+
+@pytest.mark.django_db
+def test_contact_put_request(client, restaurant, contact, contact_info):
+    restaurant = restaurant('bk', 'fastfood')
+    contact = contact(restaurant.data.get("id"))
+    data = contact_info(restaurant.data.get("id"))[0]
+    data.update({'information': '@bkff'})
+    response = client.put(f'/restaurant/contact/{contact.data[0].get("id")}/', data)
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_contact_patch_request(client, restaurant, contact):
+    restaurant = restaurant('bk', 'fastfood')
+    contact = contact(restaurant.data.get("id"))
+    response = client.patch(f'/restaurant/contact/{contact.data[0].get("id")}/', {'information': '@bkff'})
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_contact_delete_request(client, restaurant, contact):
+    restaurant = restaurant('bk', 'fastfood')
+    contact = contact(restaurant.data.get("id"))
+    response = client.delete(f'/restaurant/contact/{contact.data[0].get("id")}/')
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
